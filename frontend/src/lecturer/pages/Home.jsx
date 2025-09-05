6e11b7c5d397a904b9846c94c23adb93fe1040a1
Xabiso <spencermania480@gmail.com>
2025-09-05 18:02:21 +0200

    added headers

--- frontend/src/lecturer/pages/Home.jsx
+++ frontend/src/lecturer/pages/Home.jsx
@@ -1,9 +1,29 @@
 // Lectuer Home page with their courses// Student Home page with their courses
+import { Link } from "react-router-dom";
+import "../../components/Header.css";
 
-function LecCourses(){
-    return(
-        <h1>This page is the lecutuer Home page</h1>
-    );
+function Header() {
+  return (
+    <header className="header">
+      <Link className="logo" to="/lecturer/pages/Home">
+        Mavix
+      </Link>
+
+      <div></div>
+      <div className="header-right">
+        <i className="bx bxs-user"></i>
+        <i className="bx bxs-bell"></i>
+      </div>
+    </header>
+  );
+}
+function LecCourses() {
+  return (
+    <div>
+      <Header />
+      <h1>This page is the lecturer Home page</h1>
+    </div>
+  );
 }
 
 export default LecCourses;
--- frontend/src/student/pages/Home.jsx
+++ frontend/src/student/pages/Home.jsx
@@ -1,9 +1,30 @@
 // Student Home page with their courses
+import { Link } from "react-router-dom";
+import "../../components/Header.css";
 
-function StuCourses(){
-    return(
-        <h1>This page is the Student Home page</h1>
-    );
+function Header() {
+  return (
+    <header className="header">
+      <Link className="logo" to="/student/pages/Home">
+        Mavix
+      </Link>
+
+      <div></div>
+      <div className="header-right">
+        <i className="bx bxs-user"></i>
+        <i className="bx bxs-bell"></i>
+      </div>
+    </header>
+  );
+}
+function StuCourses() {
+  return (
+    <>
+      <Header />
+      <h1>This page is the Student Home page</h1>
+      
+    </>
+  );
 }
 
 export default StuCourses;
