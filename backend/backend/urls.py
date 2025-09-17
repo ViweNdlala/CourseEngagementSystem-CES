9deb5b5c59a984f19f689792fd5fbe11929aed3b
Xabiso <spencermania480@gmail.com>
2025-09-17 14:43:10 +0200

    migrations

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,13 +19,20 @@ from django.urls import path, include
 from accounts.views import UserView
 from courses.views import CourseView , EnrollmentView ,CourseDetailView,GeofenceSessionCreateView, GeofenceActiveView,GeofenceCheckAccessView
 from attendance.views import StudentAttendanceView, LecturerAttendanceView, AttendanceDeleteView
+from preparation.views import PreparationWeekListView, PreparationWeekDetailView, PreparationResourceListView, PreparationResourceDetailView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', UserView.as_view(), name = "user"),
+    path('enrollments/', EnrollmentView.as_view(), name='enroll-student'), 
     path('courses/', CourseView.as_view(), name='courses'),
     path('courses/<int:pk>/', CourseDetailView.as_view(), name='course-detail'),
-    path('enrollments/', EnrollmentView.as_view(), name='enroll-student'), 
+    path('courses/<int:course_id>/preparation/weeks/', PreparationWeekListView.as_view(), name='preparation-weeks'),
+    path('courses/<int:course_id>/preparation/weeks/<int:id>/', PreparationWeekDetailView.as_view(), name='preparation-week-detail'),
+    path('courses/<int:course_id>/preparation/weeks/<int:week_id>/resources/', PreparationResourceListView.as_view(), name='preparation-resources'),
+    path('courses/<int:course_id>/preparation/weeks/<int:week_id>/resources/<int:id>/', PreparationResourceDetailView.as_view(), name='preparation-resource-detail'),
+
     path('attendance/student/', StudentAttendanceView.as_view(), name='student_attendance'),
     path('attendance/lecturer/', LecturerAttendanceView.as_view(), name='lecturer_attendance'),
     path('attendance/<int:pk>/', AttendanceDeleteView.as_view(), name='attendance_delete'),
