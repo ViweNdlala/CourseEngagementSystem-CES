cff9f2fe57bad13d169af6a7c5403204183c7e9d
Viwe Ndlala <viwendlala806@gmai.com>
2025-09-18 16:59:21 +0200

    About to add syles for quizzes

--- frontend/src/lecturer/pages/Quizzes.jsx
+++ frontend/src/lecturer/pages/Quizzes.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useCourse } from "../../contexts/CourseContext";
 import { useUser } from "../../contexts/UserContext";
+import "../styles/Quizzes.css";
 
 export default function Quizzes() {
   const { currentCourse } = useCourse();
